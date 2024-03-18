6dfa55e676ea4eb6dc3278222bb53a17bb5a9342
Oliver Blass <114983621+oblassgit@users.noreply.github.com>
2024-03-18 15:26:42 +0100

    Update temp_monitoring_with_display.ino
    german solution for girls day 2024

--- temp_monitoring_with_display.ino
+++ temp_monitoring_with_display.ino
@@ -3,68 +3,69 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-#define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 32 
+#define SCREEN_WIDTH 128 // Die Breite des Displays in Pixeln
+#define SCREEN_HEIGHT 32 // Die Länge des Displays in Pixeln
 
 #define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
 #define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-#include "DHT.h" 
-#define DHTPIN 14 
-#define DHTTYPE DHT11 
+#include "DHT.h" //bindet die DHT Bibliothek ein damit der Mikrocontroller mit dem Sensor sprechen kann
+#define DHTPIN 14 //legt fest an welchem GPIO Pin der Temperatursensor hängt D1 ist in diesem Fall GPIO 5
+#define DHTTYPE DHT11 //Der Sensortyp ist DHT 11 (gibt auch noch DHT 22)
 
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(115200); //legt die Geschwindigkeit der USB Leitung fest
 
   if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
     Serial.println(F("SSD1306 allocation failed"));
-    for(;;);
+    for(;;); // Don'temperatur proceed, loop forever
   }
 
-  display.display();
+  display.display(); //zeigt den Adafruit Splashscreen
   delay(2000);
-  display.clearDisplay();
+  display.clearDisplay(); //löscht alles was vorher auf dem Display angezeigt wurde
 
-  dht.begin();
+  dht.begin(); //startet den Temperatursensor
 
 }
 
 
 void loop() {
-  delay(2000);
-  float hum = dht.readHumidity();
-  float temp = dht.readTemperature();
+  delay(2000); // 2000ms = 2s warten
+  // Variable in der wir die Luftfeuchtigkeit speichern
+  float feuchtigkeit = dht.readHumidity();
+  // Variable in der wir die Temperatur in Celsius speichern
+  float temperatur = dht.readTemperature();
 
-  if (isnan(hum) || isnan(temp)) {
+  // Hier wird geprüft, ob die Temperaturmessung fehlgeschlagen ist
+  if (isnan(feuchtigkeit) || isnan(temperatur)) {
     Serial.println(F("Lesen der Daten vom DHT Sensor ist fehlgeschlagen!"));
     display.clearDisplay();
     display.setCursor(0, 0);
     display.println("Read Failure!");
     display.display();
-    return;
+    return; // Hiermit brechen wir die Schleife ab
   }
 
-  // Logging the temp and humidity via the serial interface
   Serial.print(F("Humidity: "));
-  Serial.print(hum);
+  Serial.print(feuchtigkeit);
   Serial.print(F("%  Temperature: "));
-  Serial.print(temp);
+  Serial.print(temperatur);
   Serial.println(F("°C "));
 
 
-  display.setTextSize(1);
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(0, 0); 
-  display.cp437();
+  display.setTextSize(1); // Textgröße wird gesetzt
+  display.setTextColor(SSD1306_WHITE); // Textfarbe wird gesetzt
+  display.setCursor(0, 0); // Von wo aus angefangen wird zu schreiben (hier von oben links)
+  display.cp437(); // legt den Zeichensatz fest, mit dem wir Text ausgeben
 
-  // Showing the temp and humidity on the display
-  display.clearDisplay(); 
-  display.print(temp);
-  display.println(F(" Celsius")); // cp437 charset doesn't have "°", so "Celsius" is used.
-  display.print(hum);
+  display.clearDisplay(); // Vorherige Anzeige wird gelöscht
+  display.print(temperatur);
+  display.println(F(" Celsius"));
+  display.print(feuchtigkeit);
   display.println(" %");
-  display.display(); 
+  display.display(); // Wir zeigen an, was wir angegeben haben
 }
